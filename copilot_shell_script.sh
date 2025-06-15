2e3d95e8784d673620993b9671503e048709896d
root <e.leyian@alustudent.com>
2025-06-15 14:20:40 -0700

    summative

--- copilot_shell_script.sh
+++ copilot_shell_script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+read -p "Enter your username: " username
 read -p "Enter the new assignment name:" new_assignment
 
-CONFIG_FILE=/root/summative/submission_reminder_erin/config/config.env
+app_dir="submission_reminder_$username"
+CONFIG_FILE="$app_dir/config/config.env"
 if [[ ! -f "$CONFIG_FILE" ]]; then
 	echo "Error "$CONFIG_FILE" not found"
 	exit 1
@@ -16,8 +18,8 @@ echo "Assignment name has been changed to "$new_assignment" in "$CONFIG_FILE""
 read -p "Do you want to run startup.sh now? (y/n):" answer
 
 if [[ "$answer" =~ ^[Yy]$ ]]; then
-	if [[ -f "/root/summative/submission_reminder_erin/startup.sh" ]]; then
-		bash /root/summative/submission_reminder_erin/startup.sh
+	if [[ -f "submission_reminder_$username/startup.sh" ]]; then
+		bash submission_reminder_$username/startup.sh
 	else
         	echo "startup.sh not found in the current directory"
         fi
