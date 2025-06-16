6256225036ef2724b2ba3fd015283b60fe92a811
root <e.leyian@alustudent.com>
2025-06-16 08:51:16 -0700

    deleting

--- submission_reminder_erin/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-app_dir="/root/summative/submission_reminder_app_erin-leyian"
-
-chmod +x "submission_reminder_erin/app/reminder.sh"
-chmod +x "submission_reminder_erin/modules/functions.sh"
-bash "submission_reminder_erin/app/reminder.sh"
